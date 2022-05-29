566544885dde94dd51592cc07a3bef8d2ca7a54c
Noam Paz <noam_paz@amat.com>
2022-05-29 18:23:29 +0300

    init commit

---
+++ 11.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import requests
+
+
+def url_caller(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"{url} is ok")
+    print(datetime.now())
+
+
+for url in ["https://github.com", "https://google.com"]:
+    url_caller(url)
